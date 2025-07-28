0cbfd310c8c072f50b7798060627d3e4d9dbda01
Debaraj-stha <dstha221@gmail.com>
2025-07-28 14:01:29 +0545

    adding read me file and updating database connectin string

--- react-blog-app-server/conn.js
+++ react-blog-app-server/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 
-const conn=mongoose.connect("mongodb+srv://jeevanstha989:eCcGUVQIe4PgYKZU@cluster0.elgbq5e.mongodb.net/").then((_)=>{
+const conn=mongoose.connect(process.env.MONGO_URL).then((_)=>{
     console.log("successfully connected to database")
 }).catch((e)=>{
     console.log("error connetcing to database",e)
